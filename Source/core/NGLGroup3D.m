d3017166427eeff11d5820b073cbaaf5b78fb373
Diney Bomfim <dineybomfim@gmail.com>
2015-07-25 17:57:48 -0300

    License

--- Source/core/NGLGroup3D.m
+++ Source/core/NGLGroup3D.m
@@ -1,16 +1,23 @@
 /*
- *	NGLGroup3D.m
- *	
- *	The NinevehGL Engine is a free 3D engine to work with OpenGL programmable pipeline, made with pure
- *	Objective-C for iOS (iPhone, iPad and iPod Touch). NinevehGL is prepared to import 3D file with the
- *	formats:
- *		- WaveFront OBJ file (.obj);
- *		- COLLADA file (.dae).
+ *	Copyright (c) 2011-2015 NinevehGL. More information at: http://nineveh.gl
  *
- *	More information at the official web site: http://nineveh.gl
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
  *
- *	Created by Diney Bomfim on 11/21/11.
- *	Copyright (c) 2010 DB-Interactively. All rights reserved.
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *	THE SOFTWARE.
  */
 
 #import "NGLGroup3D.h"
--- Source/math/NGLVector.m
+++ Source/math/NGLVector.m
@@ -1,16 +1,23 @@
 /*
- *	NGLVector.h
- *	
- *	The NinevehGL Engine is a free 3D engine to work with OpenGL programmable pipeline, made with pure
- *	Objective-C for iOS (iPhone, iPad and iPod Touch). NinevehGL is prepared to import 3D file with the
- *	formats:
- *		- WaveFront OBJ file (.obj);
- *		- COLLADA file (.dae).
+ *	Copyright (c) 2011-2015 NinevehGL. More information at: http://nineveh.gl
  *
- *	More information at the official web site: http://nineveh.gl
- *	
- *	Created by Diney Bomfim on 4/9/11.
- *	Copyright (c) 2011 DB-Interactively. All rights reserved.
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *	THE SOFTWARE.
  */
 
 #import "NGLVector.h"
